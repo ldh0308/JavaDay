eabd6f284d6fcce7fa719c22341bc8e93d29ace0
이동혁 <ldh0308@naver.com>
2023-11-22 16:00:08 +0900

    If문

---
+++ src/Day03_Exception/MainEntry.java
@@ -0,0 +1,16 @@
+package Day03_Exception;
+
+public class MainEntry {
+    public static void main(String[] args) {
+        int x = 30, y = 0;
+        int result = 0;
+
+        if( y == 0 ) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return;
+        } else {
+            result = x / y; //예외발생
+        }
+        System.out.println();
+    }
+}

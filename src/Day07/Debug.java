f862f252a034ca05a958f5f828b8411d8eb0a7f5
이동혁 <ldh0308@naver.com>
2023-11-30 09:38:52 +0900

    [add] 디버깅연습

---
+++ src/Day07/Debug.java
@@ -0,0 +1,18 @@
+package Day07;
+
+public class Debug {
+    public static void main(String[] args) {
+        // 디버깅 방법
+        int x = 5, y = 0;
+        int i;
+
+        for (i = 0; i < 6; i++) {
+            y += x * i;
+        }
+        for (i = 1; i < 100; i++) {
+            if ( i == 30 ) break;
+            System.out.println(i);
+        }
+        System.out.println( x + ", " + y + ", " + i);
+    }
+}

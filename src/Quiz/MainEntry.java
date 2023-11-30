b4a3d96c1097cf9219fafb556bded954ec2b7348
이동혁 <ldh0308@naver.com>
2023-11-30 09:40:36 +0900

    [add] Inheritance(상속)

---
+++ src/Quiz/MainEntry.java
@@ -0,0 +1,12 @@
+package Quiz;
+
+public class MainEntry {
+    public static void main(String[] args) {
+        Tv tv = new Tv();
+        tv.disp();
+        tv.setColor("black");
+        tv.setChannel(7);
+        tv.disp();
+
+    }
+}
